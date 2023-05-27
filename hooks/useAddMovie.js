c3d33e4b6d2fea9ddb2118b0c040269b554f7b62
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-05-27 15:27:25 +0300

    update: custom hook add movie

---
+++ hooks/useAddMovie.js
@@ -0,0 +1,40 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState, useEffect } from "react";
+
+export default function useGetMovies({ body }) {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    const postData = async () => {
+      try {
+        const token = await AsyncStorage.getItem("token");
+        await fetch("http://10.0.2.2:5007/api/Movies", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: {
+            title: body.title,
+            imageURL: body.img,
+            description: body.desc,
+            price: body.price,
+            genre: body.genres,
+            datePublished: Date.now(),
+            actors: body.actors,
+            cinemaId: body.cinema,
+            producerId: body.producer,
+          },
+        })
+          .then((res) => res.json())
+          .then((json) => setResult(json));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    postData();
+  }, []);
+
+  return result;
+}
